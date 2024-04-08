3a94ad83ae1da6e817d2d2c64ec7cbc9a558ef36
aoiorio <130330538+aoiorio@users.noreply.github.com>
2024-04-08 17:55:57 +0900

    🍕 add: validation values and HTTPException

--- books2.py
+++ books2.py
@@ -1,7 +1,8 @@
 # Please ignore this yellow error, because you might have your virtual environment by using Docker
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
+from starlette import status
 
 app = FastAPI()
 
@@ -12,13 +13,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
 class BookRequest(BaseModel):
@@ -27,26 +30,59 @@ class BookRequest(BaseModel):
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
-    # grater than 0, and lesser than 6
+    # grater than 0, and lesser than 6, we'll accept 1 to 5
     rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(gt=1999, lt=2031)
+
+    class Config:
+        # We can remove the id.
+        json_schema_extra = {
+            "example": {
+                "title": "A new book",
+                "author": "codingwithroby",
+                "description": "A new description of a book",
+                "rating": 5,
+                "published_date": 2008,
+            }
+        }
 
 
 BOOKS = [
-    Book(1, "Computer Science Pro", "codingwithroby", "A very nice book!", 5),
-    Book(3, "Be Fast with FastAPI", "codingwithroby", "A very great book!", 5),
-    Book(4, "Master Endpoints", "codingwithroby", "A very awesome book!", 5),
-    Book(4, "HP1", "Author 1", "Book Description", 2),
-    Book(5, "HP2", "Author 2", "Book Description", 3),
-    Book(6, "HP3", "Author 3", "Book Description", 1),
+    Book(1, "Computer Science Pro", "codingwithroby", "A very nice book!", 5, 2030),
+    Book(3, "Be Fast with FastAPI", "codingwithroby", "A very great book!", 5, 2020),
+    Book(4, "Master Endpoints", "codingwithroby", "A very awesome book!", 5, 2020),
+    Book(4, "HP1", "Author 1", "Book Description", 2, 1973),
+    Book(5, "HP2", "Author 2", "Book Description", 3, 2008),
+    Book(6, "HP3", "Author 3", "Book Description", 1, 2028),
 ]
 
 
-@app.get("/books")
+@app.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books():
     return BOOKS
 
 
-@app.post("/create_book")
+# get book from book id with path validation, because it's using path parameter
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
+async def read_book(book_id: int = Path(gt=0)):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+    raise HTTPException(status_code=404, detail="Item not found")
+
+
+# get stands for searching something with query validation by using Query, because it's using query parameter.
+@app.get("/books/", status_code=status.HTTP_200_OK)
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+# post stands for creating something new
+@app.post("/create_book", status_code=status.HTTP_201_CREATED)
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.dict())
     print(type(new_book))
@@ -59,5 +95,51 @@ def find_book_id(book: Book):
     #     book.id = BOOKS[-1].id + 1
     # else:
     #     book.id = 1
-
     return book
+
+
+# update book information by using BookRequest (validation values, example values)
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
+async def update_book(book: BookRequest):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+
+# path parameter with validation
+@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            book_changed = True
+            break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+"""
+~~~~~~Answer of teacher~~~~~~~~
+@app.get("/books/publish/")
+async def read_books_by_publish_date(published_date: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
+
+"""
+
+
+
+@app.get("/books_from_published_date/")
+async def books_from_published_date(published_date: int = Query(gt=1999, lt=2031)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
